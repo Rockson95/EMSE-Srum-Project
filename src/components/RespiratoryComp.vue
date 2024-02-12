a792cf29ac64c2a6f946beff7c34005c4ce3c055
unknown <stage@empcarlabhc004.ujmse.local>
2024-02-12 13:06:41 +0100

    add RespiratoryComponent and fixed vue js bugs

---
+++ src/components/RespiratoryComp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <!-- Your template content here -->
+      <h1>Respiratory</h1>
+    </div>
+  </template>
+  
+  <script>
+  // Your JavaScript/TypeScript code here
+  export default {
+    // Component options
+  }
+  </script>
+  
+  <style scoped>
+  /* Your component-specific styles here */
+  </style>
+  
